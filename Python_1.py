c7a6f5ccc9ad947b3c4cd94cfdfb5d6c4bf553a8
Vikas Bisht <bvikastoo@gmail.com>
2023-02-27 15:33:39 +0000

    Changed the File name to pu

--- Python_1.py
+++ Python_1.py
@@ -1,3 +1,24 @@
 #this is first program
 
 print (r'c:\docs\navin')
+
+name = "youtube"
+print(name[0])
+print(name[0:2])
+print(name[-4:-1])
+
+
+list = [ "Vikas" , 39 , "Ireland"]
+print(list)
+print(list[0])
+
+
+nums = [1 ,15, 26, 37,18 , 20]
+name = ['Vikas' , 'Rajan', 'Alka']
+mil = [ nums , name ]
+
+nums.append(45)
+print(mil)
+print(sum(nums))
+nums.sort()
+print(nums)
