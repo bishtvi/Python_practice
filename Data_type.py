bb35fda8573c4fb9450808d0f3be4e4fb62e72a0
Vikas Bisht <bvikastoo@gmail.com>
2023-02-27 20:30:34 +0000

    data types

---
+++ Data_type.py
@@ -0,0 +1,15 @@
+### Data Types
+# None not assigned to any value
+# Numeric (int , float, complex , bool)
+# List
+# Tuple ( 2, 34 , 45, 67 )
+# Set {25 , 36 , 53, 59 }
+# String
+# Range
+# Dictionary d = {'navin': 'Samsung', 'rahul': 'Iphone' , 'kiran': Oneplus'} key: value
+
+
+
+
+
+
