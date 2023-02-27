60b2a10254e16f36b7ac3c4d4426cb494660a9ff
Vikas Bisht <bvikastoo@gmail.com>
2023-02-27 21:56:33 +0000

    data types

---
+++ Operators_python.py
@@ -0,0 +1,10 @@
+# Arithmectic operators
+# Assignment operators
+# Relational Operators ( <,>,=,!)
+# Logical Operators (or , and , NOR)
+# Unary operators
+
+#binary number conversion
+x=25
+print(bin(x))
+print(oct(x))
---
+++ swaping_variable.py
@@ -0,0 +1,26 @@
+
+#simple program to swap variable
+a = 5
+b = 6
+temp = a
+a = b
+b = temp
+
+print(a)
+print(b)
+
+# swapping with the formula
+a = a + b
+b = a - b
+a = a - b
+
+print(a)
+print(b)
+
+# simple way
+
+a,b = b,a
+
+print(a)
+print(b)
+
