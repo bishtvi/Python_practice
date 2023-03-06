bf1cab0ffcbdb6d50423b412620be44e851712c1
Vikas Bisht <bvikastoo@gmail.com>
2023-03-06 13:52:40 +0000

    If else example

---
+++ For_loop.py
@@ -0,0 +1,13 @@
+#for loop work with sequence
+
+x = ['Vikas', 39, 'Iphone']
+
+for i in x :
+    print(i)
+
+for i in range(20,40,2):
+    print(i)
+
+for i in range(40):
+    if i%5 !=0:
+        print(i)
---
+++ While_loop.py
@@ -0,0 +1,15 @@
+x = 1
+while x<=5:
+    print("Vikas")
+    x = x+1
+
+x = 5
+
+while x >=1:
+    print("Vikas ", end="")
+    y = 1
+    while y<=4:
+        print("Rocks ", end="")
+        y = y+1
+    x = x-1
+    print()
